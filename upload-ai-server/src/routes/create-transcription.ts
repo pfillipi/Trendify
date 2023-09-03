781646b93a98ad201842b43191020f4d4b471df1
Fillipi Pereira <pfillipi@gmx.de>
2023-09-03 06:30:36 +0200

    API

--- upload-ai-server/src/routes/create-transcription.ts
+++ upload-ai-server/src/routes/create-transcription.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { createReadStream } from "node:fs";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { openai } from "../lib/openai";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
   app.post("/videos/:videoId/transcription", async (req, res) => {
@@ -26,9 +27,15 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
     const videoPath = video.path;
     const audioReadStream = createReadStream(videoPath);
 
-    return {
-      videoId,
+    const response = await openai.audio.transcriptions.create({
+      file: audioReadStream,
+      model: "whisper-1",
+      language: "pt",
+      response_format: "json",
+      temperature: 0,
       prompt,
-    };
+    });
+
+    return response.text;
   });
 }
