5a0e21c9bfaf00df37125e1a95a3916e19632262
Fillipi Pereira <pfillipi@gmx.de>
2023-09-10 20:52:32 +0200

    path

--- upload-ai-server/src/routes/upload-video.ts
+++ upload-ai-server/src/routes/upload-video.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { fastifyMultipart } from "@fastify/multipart";
 import { prisma } from "../lib/prisma";
+import path from "node:path";
 
 export async function uploadVideoRoute(app: FastifyInstance) {
   app.register(fastifyMultipart, {
@@ -15,5 +16,7 @@ export async function uploadVideoRoute(app: FastifyInstance) {
     if (!data) {
       return reply.status(400).send({ error: "Mangler filinndata." });
     }
+
+    const extension = path.extname(data.filename);
   });
 }
