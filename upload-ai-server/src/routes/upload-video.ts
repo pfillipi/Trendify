96839cb57cee40ddc38547c73154998917497342
Fillipi Pereira <pfillipi@gmx.de>
2023-09-11 20:10:10 +0200

    fileSize

--- upload-ai-server/src/routes/upload-video.ts
+++ upload-ai-server/src/routes/upload-video.ts
@@ -3,7 +3,11 @@ import { fastifyMultipart } from "@fastify/multipart";
 import { prisma } from "../lib/prisma";
 
 export async function uploadVideoRoute(app: FastifyInstance) {
-  app.register(fastifyMultipart, {});
+  app.register(fastifyMultipart, {
+    limits: {
+      fileSize: 1048576,
+    },
+  });
 
   app.post("/videos", async () => {});
 }
