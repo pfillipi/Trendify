46e38a19ba471a5c8180dae67e62170ab6692ff7
Fillipi Pereira <pfillipi@gmx.de>
2023-09-12 20:01:16 +0200

    post

--- upload-ai-server/src/routes/upload-video.ts
+++ upload-ai-server/src/routes/upload-video.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 
 export async function uploadVideoRoute(app: FastifyInstance) {
-  app.post("/prompts", async () => {
+  app.post("/videos", async () => {
     const prompts = await prisma.prompt.findMany();
 
     return prompts;
