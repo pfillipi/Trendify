cacd9ff375ef08431cef2a4f328aacc9364e6647
Fillipi Pereira <pfillipi@gmx.de>
2023-09-14 17:14:41 +0200

    side bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,31 @@
+import { Github } from "lucide-react";
+import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+
 export function App() {
-  return <h1>Hei kjære mennesker i denne fantastiske verden!</h1>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            ❤️‍🔥🇳🇴Skapt med lidenskap i hjertet av Norge.🇳🇴❤️‍🔥
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1"></div>
+        <aside className="w-80"></aside>
+      </main>
+    </div>
+  );
 }
