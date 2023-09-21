4c95a43bb6a0c63474a9b5df037bacf52c0f18f1
Fillipi Pereira <pfillipi@gmx.de>
2023-09-21 09:41:22 +0200

    style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Github } from "lucide-react";
+import { Github, FileVideo, Upload } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
 
 export function App() {
   return (
@@ -48,8 +49,44 @@ export function App() {
         </div>
 
         <aside className="w-80 space-y-6">
-          <form action=""></form>
+          <form className="space-y-6">
+            <label
+              htmlFor="video"
+              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+            >
+              <FileVideo className="w-4 h-4"></FileVideo>
+              Velg en video.
+            </label>
+            <input
+              type="file"
+              id="video"
+              accept="video/mp4"
+              className="sr-only"
+            />
+            <Separator />
+
+            <div className="space-y-2">
+              <Label htmlFor="transcription_prompt">
+                Transkripsjons prompt
+              </Label>
+              <Textarea
+                id="transcription_prompt"
+                className="h-20 leading-relaxed"
+                placeholder="Vennligst inkluder nøkkelord som ble nevnt i videon, og skill dem med komma (,)"
+              />
+            </div>
+            <Button type="submit" className="w-full">
+              Lagre video
+              <Upload className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
         </aside>
+
+        <Separator />
+
+        <form className="space-y-6">
+          <div className="space-y-1"></div>
+        </form>
       </main>
     </div>
   );
