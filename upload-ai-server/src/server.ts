d023c59fc0c04da34e5929525c1cddbc6d171f95
Fillipi Pereira <pfillipi@gmx.de>
2023-09-23 16:27:24 +0200

    reference

--- upload-ai-server/src/server.ts
+++ upload-ai-server/src/server.ts
@@ -3,12 +3,6 @@ import { prisma } from "./lib/prisma";
 
 const app = fastify();
 
-app.get("/prompts", async () => {
-  const prompts = await prisma.prompt.findMany();
-
-  return prompts;
-});
-
 app
   .listen({
     port: 3333,
