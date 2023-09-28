0f68e3c4ac12299ff166ac495ca62dde243d92a2
Fillipi Pereira <pfillipi@gmx.de>
2023-09-29 01:06:08 +0200

    complete

--- upload-ai-server/src/routes/generate-ai-compleition.ts
+++ upload-ai-server/src/routes/generate-ai-compleition.ts
@@ -5,7 +5,7 @@ import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
 
 export async function generateAICompletionRoute(app: FastifyInstance) {
-  app.post("/videos/:videoId/transcription", async (req) => {
+  app.post("/ai/complete", async (req) => {
     const bodySchema = z.object({
       prompt: z.string(),
     });
