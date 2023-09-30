e566d7897fab657e04c0be3fe279c002c34d583a
Fillipi Pereira <pfillipi@gmx.de>
2023-10-01 01:15:28 +0200

    default

--- upload-ai-server/src/routes/generate-ai-compleition.ts
+++ upload-ai-server/src/routes/generate-ai-compleition.ts
@@ -9,7 +9,7 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
       template: z.string(),
-      temperature: z.number().min(0).max(1),
+      temperature: z.number().min(0).max(1).default(0.5),
     });
 
     const { prompt } = bodySchema.parse(req.body);
