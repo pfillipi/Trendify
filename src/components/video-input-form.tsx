12b99db6fa811220bf89c28215599d7d2cd6c6fe
Fillipi Pereira <pfillipi@gmx.de>
2023-10-08 15:37:22 +0200

    previewURL

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -30,10 +30,10 @@ export function VideInputForm() {
     <form className="space-y-6">
       <label
         htmlFor="video"
-        className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+        className="border  flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
       >
-        {videoFile ? (
-          "hei"
+        {previewURL ? (
+          <video src={previewURL} controls={false} className="" />
         ) : (
           <>
             <FileVideo className="w-4 h-4"></FileVideo>
